cdac3c1d3382d33ab11fb2c986601395448e4feb
Mjosh47 <joshua.munyirwa@strathmore.edu>
2024-10-05 14:06:38 +0300

    css to bs

--- structure/contents.php
+++ structure/contents.php
@@ -18,7 +18,7 @@ class contents{
     public function about_content(){
         ?>
         <div class="row align-items-md-stretch">
-              <div class="col-md-
+              <div class="col-md-8">
                 <h1>About Us</h1>
                 <p>
                 <img src="images/yankrukov.jpg" class="abt_img" />Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
@@ -35,11 +35,13 @@ class contents{
     }
     public function sidebar(){
         ?>
-                <div class="sidebar">
+                 <div class="col-md-4">
+                 <div class="h-100 p-5 bg-body-tertiary border rounded-3">
                     <h1>Side Bar</h1>
                     <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
+                 </div>
                 </div>
             </div>
         <?php
