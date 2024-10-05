b8253efe8c49eded67e5f2b2f3a2136655623b64
Mjosh47 <joshua.munyirwa@strathmore.edu>
2024-10-05 13:47:55 +0300

    func update

--- structure/layouts.php
+++ structure/layouts.php
@@ -12,7 +12,22 @@
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
             </head>
             <body>
+                <div class = "container">
             <?php
+        }
+
+        public function banner(){
+            ?>
+            <div class="p-3 mb-4 bg-body-tertiary rounded-3">
+              <div class="container-fluid py-2">
+                <h1 class="display-5 fw-bold">Assignemnt II</h1>
+                <p class="col-md-8 fs-4">Real time operating systems for embedded systems development
+Is used to address several industry verticals such as Consumer devices, Automotive devices etc.It not only focuses on the basic Real time Operating systems but also systems like Priority Inversion, Advanced Memory Protection and Error Management. It also covers interesting topics such as why a Round Robin Scheduling is not required for Safety Critical Avionics Applications.
+  </p>
+                <button class="btn btn-primary btn-lg" type="button">Example button</button>
+              </div>
+            </div>
+          <?php
         }
 
         public function footer(){
