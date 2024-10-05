fea6c1405feab652322b7b75ef70a9d4c33ee9b9
Mjosh47 <joshua.munyirwa@strathmore.edu>
2024-10-05 13:40:03 +0300

    adding search bar

--- structure/menus.php
+++ structure/menus.php
@@ -29,7 +29,12 @@
                             <a class="nav-link" href="signup.php">Sign Up</a>
                           </li>
                         </ul>
-                  
+                        <span class="navbar-text">
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                              </form>
+                        </span>
                       </div>
                     </div>
                   </nav>
